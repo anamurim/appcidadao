ca81a794cb3a81095c6dce1435d850bde21075a3
anamurim <ana.amurim@gmail.com>
2026-02-21 18:45:24 -0300

    Ajustes temas

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:appcidadao/funcionalidades/autenticacao/apresentacao/paginas/tela_cadastro_usuario.dart';
 import 'package:appcidadao/funcionalidades/autenticacao/apresentacao/paginas/tela_login.dart';
 import 'package:appcidadao/funcionalidades/autenticacao/apresentacao/paginas/tela_recuperar_senha.dart';
-import 'package:appcidadao/funcionalidades/autenticacao/apresentacao/paginas/tela_cadastro_usuario.dart';
 import 'package:appcidadao/funcionalidades/home/apresentacao/paginas/home_pagina.dart';
-import 'package:appcidadao/core/tema/app_tema.dart';
-import 'package:appcidadao/funcionalidades/autenticacao/controladores/autenticacao_controller.dart';
-import 'package:appcidadao/funcionalidades/home/controladores/reporte_controller.dart';
-import 'package:appcidadao/funcionalidades/home/controladores/usuario_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+// Importe seus controllers (ajuste os caminhos conforme seu projeto)
+import 'core/tema/app_tema.dart';
+import 'core/tema/tema_controller.dart';
+import 'funcionalidades/autenticacao/controladores/autenticacao_controller.dart';
+
+// Certifique-se de importar estes abaixo:
+import 'funcionalidades/home/controladores/usuario_controller.dart';
+import 'funcionalidades/home/controladores/reporte_controller.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const AppCidadao());
 }
 
@@ -21,21 +27,28 @@ class AppCidadao extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => AutenticacaoController()),
-        ChangeNotifierProvider(
-          create: (_) => UsuarioController()..carregarUsuario(),
-        ),
+
+        ChangeNotifierProvider(create: (_) => UsuarioController()),
         ChangeNotifierProvider(create: (_) => ReporteController()),
       ],
-      child: MaterialApp(
-        title: 'App Cidadão',
-        debugShowCheckedModeBanner: false,
-        theme: AppTema.temaEscuro,
-        initialRoute: '/',
-        routes: {
-          '/': (context) => const TechLoginScreen(),
-          '/home': (context) => const HomePagina(),
-          '/forgot-password': (context) => const ForgotPasswordScreen(),
-          '/signup': (context) => const SignupScreen(),
+      child: ValueListenableBuilder<ThemeMode>(
+        valueListenable: TemaController.instance,
+        builder: (context, modoTema, _) {
+          return MaterialApp(
+            title: 'Equatorial App Cidadão',
+            debugShowCheckedModeBanner: false,
+            theme: AppTema.temaClaro,
+            darkTheme: AppTema.temaEscuro,
+            themeMode: modoTema,
+            initialRoute: '/',
+            routes: {
+              '/': (context) => const TechLoginScreen(),
+              '/signup': (context) => const SignupScreen(),
+              '/forgot-password': (context) => const ForgotPasswordScreen(),
+              //Adicione a rota da Home se necessário
+              '/home': (context) => const HomePagina(),
+            },
+          );
         },
       ),
     );
